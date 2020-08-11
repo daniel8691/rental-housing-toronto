667ccb0e26e0f92b45d29a637e6f61ead812393e
Daniel Tsai <59071439+daniel8691@users.noreply.github.com>
2020-08-11 19:49:25 -0400

    added webpages

--- database_storage.py
+++ database_storage.py
@@ -12,3 +12,10 @@ connection = sqlite3.connect("rent_toronto.db")
 toronto_df = pd.read_sql("SELECT * FROM toronto_df", connection)
 toronto_df.drop(columns = ['index'], inplace=True)
 print(toronto_df.head())
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/toronto_rental_data"
+mongo = PyMongo(app)
+
+@app.route()
